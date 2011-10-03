74a265c5a73446df13b2dfe4c996ce58486d56f6
Dimitris Bousis <whantana@gmail.com>
2011-10-03 03:45:45 +0300

    testing stuff with GeoRSS format

--- webapp/src/main/java/uberdust/controllers/ShowTestbedGeoRssController.java
+++ webapp/src/main/java/uberdust/controllers/ShowTestbedGeoRssController.java
@@ -1,42 +1,31 @@
 package uberdust.controllers;
 
-import eu.wisebed.wisedb.controller.NodeController;
 import eu.wisebed.wisedb.controller.TestbedController;
 import eu.wisebed.wisedb.model.Testbed;
-import eu.wisebed.wiseml.model.setup.Node;
 import org.apache.log4j.Logger;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractRestController;
-import uberdust.commands.NodeCommand;
+import uberdust.commands.TestbedCommand;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-/**
- * REST Controller for displaying node information.
- */
-public class ListNodesController extends AbstractRestController {
+public class ShowTestbedGeoRssController extends AbstractRestController {
 
-    private NodeController nodeManager;
     private TestbedController testbedManager;
-    private static final Logger LOGGER = Logger.getLogger(ListNodesController.class);
+    private static final Logger LOGGER = Logger.getLogger(ShowTestbedController.class);
 
-    public ListNodesController() {
+
+    public ShowTestbedGeoRssController() {
         super();
 
         // Make sure to set which method this controller will support.
         this.setSupportedMethods(new String[]{METHOD_GET});
-    }
-
-    public void setNodeManager(NodeController nodeManager) {
-        this.nodeManager = nodeManager;
     }
 
     public void setTestbedManager(final TestbedController testbedManager) {
@@ -44,28 +33,35 @@ public class ListNodesController extends AbstractRestController {
     }
 
     @Override
-    protected ModelAndView handle(HttpServletRequest request,
-                                  HttpServletResponse response, Object commandObj, BindException errors)
-            throws Exception {
+    protected ModelAndView handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                  Object commandObj, BindException e) throws Exception {
 
-        // get command object
-        NodeCommand command = (NodeCommand) commandObj;
-        LOGGER.info("command.getTestbedId() : " + command.getTestbedId());
+        // set command object
+        TestbedCommand command = (TestbedCommand) commandObj;
+        LOGGER.info("commandObj.getTestbedId() : " + command.getTestbedId());
 
-        // fetch testbed
+        // a specific testbed is requested by testbed Id
+        int testbedId;
+        try {
+            testbedId = Integer.parseInt(command.getTestbedId());
+
+        } catch (NumberFormatException nfe) {
+            throw new Exception(new Throwable("Testbed IDs have number format."));
+        }
+
+        // look up testbed
         Testbed testbed = testbedManager.getByID(Integer.parseInt(command.getTestbedId()));
-
-        // List of nodes
-        //List<Node> nodes = nodeManager.listTestbedNodes(testbed);
-        List<Node> nodes = nodeManager.list();
+        if (testbed == null) {
+            // if no testbed is found throw exception
+            throw new Exception(new Throwable("Cannot find testbed [" + testbedId + "]."));
+        }
 
         // Prepare data to pass to jsp
         final Map<String, Object> refData = new HashMap<String, Object>();
 
         // else put thisNode instance in refData and return index view
-        refData.put("testbedId", command.getTestbedId());
-        refData.put("nodes", nodes);
-        return new ModelAndView("node/list.html", refData);
+        refData.put("testbed", testbed);
+        return new ModelAndView("testbed/geo.rss", refData);
     }
 
     @ExceptionHandler(Exception.class)
