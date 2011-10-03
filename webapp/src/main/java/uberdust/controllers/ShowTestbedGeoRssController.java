e6b80b5e1682a687d9e96b8e91c31bfc603ef0e4
Dimitris Bousis <whantana@gmail.com>
2011-10-03 18:27:14 +0300

    working on the georss format. Must fix the point alignment

--- webapp/src/main/java/uberdust/controllers/ShowTestbedGeoRssController.java
+++ webapp/src/main/java/uberdust/controllers/ShowTestbedGeoRssController.java
@@ -8,6 +8,8 @@ import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedOutput;
 import eu.wisebed.wisedb.controller.TestbedController;
 import eu.wisebed.wisedb.model.Testbed;
+import eu.wisebed.wiseml.model.setup.Capability;
+import eu.wisebed.wiseml.model.setup.Node;
 import org.apache.log4j.Logger;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -65,27 +67,48 @@ public class ShowTestbedGeoRssController extends AbstractRestController {
         httpServletResponse.setContentType("application/xml; charset=UTF-8");
         SyndFeed feed = new SyndFeedImpl();
         feed.setFeedType("rss_2.0");
-        feed.setTitle(testbed.getName()  + " GeoRSS");
+        feed.setTitle(testbed.getName() + " GeoRSS");
         feed.setLink(httpServletRequest.getRequestURL().toString());
         feed.setDescription(testbed.getDescription());
         List<SyndEntry> entries = new ArrayList<SyndEntry>();
 
         // make an entry and it
-        SyndEntry entry = new SyndEntryImpl();
-        entry.setTitle(testbed.getName()  + " GeoRSS");
-        entry.setLink(httpServletRequest.getRequestURL().toString());
-        entry.setPublishedDate(new Date());
-        SyndContent description = new SyndContentImpl();
-        description.setType("text/plain");
-        description.setValue(testbed.getName() + " GeoRSS");
-        entry.setDescription(description);
-        entries.add(entry);
+        for (Node node : testbed.getSetup().getNodes()) {
+            SyndEntry entry = new SyndEntryImpl();
 
-        // set the GeoRSS module and add it
-        GeoRSSModule geoRSSModule = new SimpleModuleImpl();
-        geoRSSModule.setPosition(new Position(testbed.getSetup().getOrigin().getX(),
-                testbed.getSetup().getOrigin().getY()));
-        entry.getModules().add(geoRSSModule);
+            // set entry's title,link and publishing date
+            entry.setTitle(node.getId());
+            entry.setLink("http://150.140.5.11:8080" +     // TODO those constants should get out.They suck
+                    "/uberdust/rest/testbed/" + testbed.getId() + "/node/" + node.getId());
+            entry.setPublishedDate(new Date());
+
+            // set entry's description (HTML list)
+            SyndContent description = new SyndContentImpl();
+            StringBuffer descriptionBuffer = new StringBuffer();
+            descriptionBuffer.append("<p>" + node.getDescription() + "</p>");
+            descriptionBuffer.append("<ul>");
+            for(Capability capability : node.getCapabilities()) {
+                descriptionBuffer.append("<li><a href=\"http://150.140.5.11:8080" +
+                        "/uberdust/rest/testbed/" + testbed.getId() +
+                        "/node/" + node.getId() + "/capability/" + capability.getName() +"\">"+ capability.getName()
+                        + "</a></li>"
+                );
+            }
+            descriptionBuffer.append("</ul>");
+            description.setType("text/html");
+            description.setValue(descriptionBuffer.toString());
+            entry.setDescription(description);
+
+            // set the GeoRSS module and add it
+            GeoRSSModule geoRSSModule = new SimpleModuleImpl();
+            geoRSSModule.setPosition(new Position(
+                    testbed.getSetup().getOrigin().getX() - node.getPosition().getX(),
+                    testbed.getSetup().getOrigin().getY() - node.getPosition().getY()));
+            entry.getModules().add(geoRSSModule);
+            entries.add(entry);
+        }
+
+        // add entries to feed
         feed.setEntries(entries);
 
         // the feed output goes to response
