d8856b0b9e24eaba247af5468d1bc0b864864e97
Orestis Akribopoulos <akribopo@cti.gr>
2011-10-12 12:32:46 +0300

    Working on O.I.11 app

---
+++ applications/0.I.11/src/main/java/uberdust/lights/timers/LastReadingTask.java
@@ -0,0 +1,69 @@
+package uberdust.lights.timers;
+
+import org.apache.log4j.Logger;
+import uberdust.communication.RestClient;
+import uberdust.lights.LightController;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akribopo
+ * Date: 10/10/11
+ * Time: 11:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LastReadingTask extends TimerTask {
+
+    private final static Logger LOGGER = Logger.getLogger(LastReadingTask.class);
+
+    public static final int DELAY = 500;
+
+    final Timer timer;
+
+    public LastReadingTask(final Timer timer) {
+        super();
+        this.timer = timer;
+    }
+
+    @Override
+    public void run() {
+
+        final long previousPirEvent = LightController.getInstance().getLastReading();
+
+        final long lastPirEvent = LightController.getInstance().lastPirEvent();
+
+        if (System.currentTimeMillis() - lastPirEvent > 5000) {
+            LOGGER.debug("Too old Pir Event");
+            return;
+        }
+
+        if (lastPirEvent - previousPirEvent > 0) {
+            if (!LightController.getInstance().isZone1()) {
+                //turn on Zone 1;
+                LightController.getInstance().controlLight(true, 1);
+                LOGGER.debug("Turn On Zone 1");
+                LightController.getInstance().setZone1(true);
+                LightController.getInstance().setLastZone1Reading(lastPirEvent);
+                //Start Timer to Turn off the Lights.
+                timer.schedule(new LightTask(timer), LightTask.DELAY);
+
+            } else if (LightController.getInstance().isZone1()) {
+                if (lastPirEvent - LightController.getInstance().getLastZone1Reading() > 15000) {
+                    if (!LightController.getInstance().isZone2()) {
+                        //turn on Zone 2;
+                        //RestClient.getInstance().callRestfulWebService();
+                        LightController.getInstance().controlLight(true, 2);
+                        LOGGER.debug("Turn On Zone 2");
+                        LightController.getInstance().setZone2(true);
+                        LightController.getInstance().setLastZone2Reading(lastPirEvent);
+                    }
+                }
+            }
+            LightController.getInstance().setLastReading(lastPirEvent);
+        }
+    }
+}
