cf7dbe65656101d75005713425ae05508afcda24
Orestis Akribopoulos <akribopo@cti.gr>
2011-11-08 00:48:34 +0200

    Implementing /lastreading WebSocket

---
+++ webapp/src/main/java/eu/uberdust/websockets/CustomWebSocketListener.java
@@ -0,0 +1,116 @@
+package eu.uberdust.websockets;
+
+import com.caucho.websocket.AbstractWebSocketListener;
+import com.caucho.websocket.WebSocketContext;
+import eu.wisebed.wisedb.listeners.LastNodeReadingObservable;
+import eu.wisebed.wisedb.model.NodeReading;
+import org.apache.log4j.Logger;
+
+import javax.persistence.metamodel.StaticMetamodel;
+import javax.xml.soap.Node;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akribopo
+ * Date: 11/8/11
+ * Time: 12:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomWebSocketListener extends AbstractWebSocketListener implements Observer {
+
+    /**
+     * Static Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(CustomWebSocketListener.class);
+
+    /**
+     * A List with the connected users.
+     */
+    private final List<WebSocketContext> users = new ArrayList<WebSocketContext>();
+
+    /**
+     * The protocol which been handled from the specific listener.
+     */
+    private final String thisProtocol;
+
+    /**
+     * Constructor.
+     *
+     * @param protocol protocol ID.
+     */
+    public CustomWebSocketListener(final String protocol) {
+        super();
+        thisProtocol = protocol;
+        LastNodeReadingObservable.getInstance().addObserver(this);
+    }
+
+    @Override
+    public void onStart(final WebSocketContext context) throws IOException {
+        super.onStart(context);
+        users.add(context);
+        LOGGER.info("onStart");
+    }
+
+    @Override
+    public void onReadBinary(final WebSocketContext context, final InputStream is) throws IOException {
+        super.onReadBinary(context, is);
+    }
+
+    @Override
+    public void onReadText(final WebSocketContext context, final Reader is) throws IOException {
+        super.onReadText(context, is);
+    }
+
+    @Override
+    public void onClose(final WebSocketContext context) throws IOException {
+        super.onClose(context);
+        LOGGER.info("onClose");
+        users.remove(context);
+    }
+
+    @Override
+    public void onDisconnect(final WebSocketContext context) throws IOException {
+        super.onDisconnect(context);
+        LOGGER.info("onDisconnect");
+        users.remove(context);
+    }
+
+    @Override
+    public void onTimeout(final WebSocketContext context) throws IOException {
+        super.onTimeout(context);
+        LOGGER.info("onTimeout");
+        users.remove(context);
+    }
+
+
+    @Override
+    public void update(final Observable o, final Object arg) {
+        if (o instanceof LastNodeReadingObservable && arg instanceof NodeReading) {
+            final NodeReading lastReading = (NodeReading) arg;
+
+            if ((new StringBuilder().append(lastReading.getCapability().getName()).append(":").append(lastReading.getNode().getId()).toString())
+                    .equals(thisProtocol)) {
+
+                final String response = new StringBuilder().append(lastReading.getTimestamp()).append(":").append(lastReading.getReading()).toString();
+                LOGGER.info(response);
+                for (final WebSocketContext user : users) {
+                    try {
+                        final PrintWriter thisWriter = user.startTextMessage();
+                        thisWriter.println(response);
+                        thisWriter.close();
+                    } catch (final IOException e) {
+                        LOGGER.error(e);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ webapp/src/main/java/eu/uberdust/websockets/LastReadingWebSocket.java
@@ -0,0 +1,74 @@
+package eu.uberdust.websockets;
+
+import com.caucho.websocket.WebSocketServletRequest;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Validates the initial HTTP request and  dispatches a new WebSocket connection.
+ */
+public class LastReadingWebSocket extends GenericServlet {
+
+    /**
+     * A HashMap<CapabilityID:NodeID>.
+     */
+    private final HashMap<String, CustomWebSocketListener> listeners;
+
+    /**
+     * Default Constructor.
+     */
+    public LastReadingWebSocket() {
+        super();
+        listeners = new HashMap<String, CustomWebSocketListener>();
+    }
+
+    /**
+     * Services the request.
+     *
+     * @param servletRequest  the servletRequest
+     * @param servletResponse the servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(final ServletRequest servletRequest, final ServletResponse servletResponse) throws ServletException, IOException {
+        final HttpServletRequest req = (HttpServletRequest) servletRequest;
+        final HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        /*
+        * Process the handshake, selecting the protocol to be used.
+        * The protocol is Defined by: capabilityID:NodeID
+        */
+        final String protocol = req.getHeader("Sec-WebSocket-Protocol");
+
+        /**
+         * TODO: FIX this check.
+         */
+        if (protocol == null) {
+            res.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE);
+            return;
+        }
+
+        final CustomWebSocketListener thisListener;
+        if (listeners.containsKey(protocol)) {
+            res.setHeader("Sec-WebSocket-Protocol", protocol);
+            thisListener = listeners.get(protocol);
+        } else {
+            thisListener = new CustomWebSocketListener(protocol);
+            listeners.put(protocol, thisListener);
+            res.setHeader("Sec-WebSocket-Protocol", protocol);
+        }
+
+        final WebSocketServletRequest wsRequest = (WebSocketServletRequest) servletRequest;
+        wsRequest.startWebSocket(thisListener);
+    }
+
+}
+
