dd6458cb3d38548a24b5d70b6789c3426750d903
Orestis Akribopoulos <akribopo@cti.gr>
2011-11-14 16:21:40 +0200

    Adding WebSocket Client

---
+++ applications/0.I.11WS/src/main/java/eu/uberdust/communication/websocket/WebSocketIMPL.java
@@ -0,0 +1,54 @@
+package eu.uberdust.communication.websocket;
+
+import eu.uberdust.lights.LightController;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.websocket.WebSocket;
+
+import java.util.Date;
+
+/**
+ * The Implementation of the WebSocket Interface.
+ */
+public class WebSocketIMPL implements WebSocket.OnTextMessage {
+
+    /**
+     * Static Logger.
+     */
+    private final static Logger LOGGER = Logger.getLogger(WebSocketIMPL.class);
+
+    /**
+     * Called with a complete text message when all fragments have been received.
+     * The maximum size of text message that may be aggregated from multiple frames is set with {@link Connection#setMaxTextMessageSize(int)}.
+     *
+     * @param data The message
+     */
+    @Override
+    public void onMessage(final String data) {
+        LOGGER.info(new StringBuilder().append("-- onMessage: ").append(data).append(new Date()).toString());
+        LightController.getInstance().setLastReading(System.currentTimeMillis());
+        //LightController.getInstance().setLastReading(Long.parseLong(data.split("\t")[0]));
+        LOGGER.info(new StringBuilder().append("-- finished : ").append(new Date()).toString());
+    }
+
+    /**
+     * Called when a new websocket connection is accepted.
+     *
+     * @param connection The Connection object to use to send messages.
+     */
+    @Override
+    public void onOpen(final Connection connection) {
+        LOGGER.info("onOpen");
+    }
+
+    /**
+     * Called when an established websocket connection closes
+     *
+     * @param closeCode the Close Code
+     * @param message   the Message
+     */
+    @Override
+    public void onClose(final int closeCode, final String message) {
+        LOGGER.info("onClose");
+        WSocketClient.getInstance().connect();
+    }
+}
