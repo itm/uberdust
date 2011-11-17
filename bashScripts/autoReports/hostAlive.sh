b465c08b6ec01155fccc54be5284afa267311507
Dimitrios <amaxilat@cti.gr>
2011-11-17 23:20:20 +0200

    add scripts for gathering host information

---
+++ bashScripts/autoReports/hostAlive.sh
@@ -0,0 +1,4 @@
+source /var/autoReports/settings
+
+addReading "status" "1"
+
---
+++ bashScripts/autoReports/memstat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /var/autoReports/settings
+
+FILE=temp
+PREFIX="MemFree"
+
+#execute commands
+cat /proc/meminfo | grep $PREFIX > $FILE
+
+#truncate data
+VAL=$(awk '{print $2;}' $FILE)
+
+addReading $PREFIX $VAL
