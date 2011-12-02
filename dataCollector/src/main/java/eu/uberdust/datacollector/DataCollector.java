1f348bf469bb52235827076286bc3df8f31da362
Dimitrios Amaxilatis <amaxilat@cti.gr>
2011-12-02 13:30:58 +0200

    add message parsers for rest and websockets

--- dataCollector/src/main/java/eu/uberdust/datacollector/DataCollector.java
+++ dataCollector/src/main/java/eu/uberdust/datacollector/DataCollector.java
@@ -1,30 +1,19 @@
 package eu.uberdust.datacollector;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import de.uniluebeck.itm.gtr.messaging.Messages;
-import de.uniluebeck.itm.tr.runtime.wsnapp.WSNApp;
-import de.uniluebeck.itm.tr.runtime.wsnapp.WSNAppMessages;
 import eu.uberdust.communication.websocket.InsertReadingWebSocketClient;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.jboss.netty.bootstrap.ClientBootstrap;
-import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
-import org.jboss.netty.handler.codec.frame.LengthFieldBasedFrameDecoder;
-import org.jboss.netty.handler.codec.frame.LengthFieldPrepender;
-import org.jboss.netty.handler.codec.protobuf.ProtobufDecoder;
-import org.jboss.netty.handler.codec.protobuf.ProtobufEncoder;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
-import static org.jboss.netty.channel.Channels.pipeline;
 
 
 /**
@@ -45,10 +34,7 @@ public class DataCollector {
      */
     private static final String PROPERTY_FILE = "dataCollector.properties";
 
-    /**
-     *
-     */
-    private static final int REPORT_LIMIT = 1000;
+
     /**
      * testbed hostname.
      */
@@ -61,19 +47,11 @@ public class DataCollector {
      * map of the names used in iSense application to capability names.
      */
     private final transient Map<String, String> sensors = new HashMap<String, String>();
-    /**
-     * counts the messages received - stats.
-     */
-    private transient int messageCounter;
-    /**
-     * saves the last time 1000 messages were received - stats.
-     */
-    private transient long lastTime;
-    /**
-     * executors for handling incoming messages.
-     */
-    private final transient ExecutorService executorService;
 
+
+    private void exceptionCaught(Exception e) {
+        LOGGER.error("caught exception aaaaaa");
+    }
 
     /**
      * Default Constructor.
@@ -83,11 +61,9 @@ public class DataCollector {
 
         readProperties();
 
-        messageCounter = 0;
-        lastTime = System.currentTimeMillis();
 
-        executorService = Executors.newCachedThreadPool();
         connectWS();
+
     }
 
     private void connectWS() {
@@ -131,94 +107,12 @@ public class DataCollector {
         LOGGER.info(devBuilder);
     }
 
-    /**
-     * Chanel handler that receives the messages and Generates parser threads.
-     */
-    private final transient SimpleChannelUpstreamHandler upstreamHandler = new SimpleChannelUpstreamHandler() {
 
-        @Override
-        public void messageReceived(final ChannelHandlerContext ctx, final MessageEvent messageEvent)
-                throws InvalidProtocolBufferException {
-            final Messages.Msg message = (Messages.Msg) messageEvent.getMessage();
-            if (WSNApp.MSG_TYPE_LISTENER_MESSAGE.equals(message.getMsgType())) {
-                final WSNAppMessages.Message wsnAppMessage = WSNAppMessages.Message.parseFrom(message.getPayload());
-                parse(wsnAppMessage.toString());
-                messageCounter++;
-                if (messageCounter == REPORT_LIMIT) {
-                    final long milliseconds = System.currentTimeMillis() - lastTime;
-                    final double stat = messageCounter / (milliseconds / (double) REPORT_LIMIT);
-                    LOGGER.info("MessageRate : " + stat + " messages/sec");
-                    final ThreadPoolExecutor pool = (ThreadPoolExecutor) executorService;
-                    LOGGER.info("PoolSize : " + pool.getPoolSize() + " Active :" + pool.getActiveCount());
-                    LOGGER.info("Peak : " + pool.getLargestPoolSize());
-
-                    lastTime = System.currentTimeMillis();
-                    messageCounter = 0;
-                }
-
-            } else {
-                LOGGER.error("got a message of type " + message.getMsgType());
-            }
-        }
-
-        /**
-         *
-         * @param ctx
-         * @param channelStateEvent
-         * @throws Exception
-         */
-        @Override
-        public void channelDisconnected(final ChannelHandlerContext ctx, final ChannelStateEvent channelStateEvent)
-                throws Exception {     //NOPMD
-            super.channelDisconnected(ctx, channelStateEvent);
-            LOGGER.error("channelDisconnected");
-            System.exit(1);
-        }
-
-        /**
-         *
-         * @param toString
-         */
-        private void parse(final String toString) {
-            executorService.submit(new MessageParser(toString, sensors));
-        }
-    };
 
     /**
      * Channel factory with custom channelPipeline to parse the received messages.
      */
-    private final transient ChannelPipelineFactory chPipelineFactory = new ChannelPipelineFactory() {
-
-        /**
-         * a decoder size limit.
-         */
-        public static final int MAX_LEN = 1048576;
-        /**
-         * constant parameter.
-         */
-        public static final int FLD_LEN = 4;
-        /**
-         * constant parameter.
-         */
-        public static final int STRIP = 4;
-
-        @Override
-        public ChannelPipeline getPipeline() {
-
-            final ChannelPipeline channelPipeline = pipeline();
-
-            channelPipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(MAX_LEN, 0, FLD_LEN, 0, STRIP));
-            channelPipeline.addLast("pbfEnvelopeMessageDec", new ProtobufDecoder(Messages.Msg.getDefaultInstance()));
-
-            channelPipeline.addLast("frameEncoder", new LengthFieldPrepender(FLD_LEN));
-            channelPipeline.addLast("protobufEncoder", new ProtobufEncoder());
-
-            channelPipeline.addLast("handler", upstreamHandler);
-
-            return channelPipeline;
-
-        }
-    };
+    private final transient DataCollectorPipelineFactory chPipelineFactory = new DataCollectorPipelineFactory(this);
 
 
     /**
@@ -229,6 +123,8 @@ public class DataCollector {
         factory = new NioClientSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
 
         final ClientBootstrap bootstrap = new ClientBootstrap(factory);
+
+        chPipelineFactory.setSensors(sensors);
 
         // Configure the event pipeline factory.
         bootstrap.setPipelineFactory(chPipelineFactory);
