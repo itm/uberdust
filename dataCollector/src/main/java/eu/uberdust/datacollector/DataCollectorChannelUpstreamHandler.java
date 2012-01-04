9dc44b9c931e62a2c3e9a053cb0cbb9c56a3298c
amaxilat <amaxilat@cti.gr>
2012-01-04 10:01:29 +0200

    reconnection to TR

--- dataCollector/src/main/java/eu/uberdust/datacollector/DataCollectorChannelUpstreamHandler.java
+++ dataCollector/src/main/java/eu/uberdust/datacollector/DataCollectorChannelUpstreamHandler.java
@@ -8,11 +8,9 @@ import eu.uberdust.datacollector.parsers.MessageParser;
 import eu.uberdust.datacollector.parsers.WsCommiter;
 import eu.uberdust.reading.NodeReading;
 import org.apache.log4j.Logger;
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.channel.MessageEvent;
-import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.jboss.netty.channel.*;
 
+import java.net.ConnectException;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -70,6 +68,13 @@ public class DataCollectorChannelUpstreamHandler extends SimpleChannelUpstreamHa
         lastTime = System.currentTimeMillis();
 
         executorService = Executors.newCachedThreadPool();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        if (e instanceof ConnectException) {
+            LOGGER.warn("ConnectException");
+        }
     }
 
     @Override
@@ -141,12 +146,12 @@ public class DataCollectorChannelUpstreamHandler extends SimpleChannelUpstreamHa
      */
     private void shutdown() {
         LOGGER.error("Shutting down!!!");
-        executorService.shutdown();
         dataCollector.restart();
     }
 
     /**
      * Submits a new thread to the executor to parse the new string message.
+     *
      * @param toString the string to parse
      */
     private void parse(final String toString) {
