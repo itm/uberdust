d91741a764f28966ef01bc2efb4572abdfaf5e59
Dimitrios Amaxilatis <amaxilat@cti.gr>
2012-01-17 12:45:03 +0200

    add properties for applications to start

--- dataCollector/src/main/java/eu/uberdust/TestbedListener.java
+++ dataCollector/src/main/java/eu/uberdust/TestbedListener.java
@@ -3,6 +3,7 @@ package eu.uberdust;
 import eu.uberdust.controller.TestbedController;
 import eu.uberdust.datacollector.DataCollector;
 import eu.uberdust.nodeflasher.NodeFlasherController;
+import eu.uberdust.util.PropertyReader;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,13 +16,20 @@ public class TestbedListener {
     private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(TestbedListener.class);
 
     public static void main(final String[] args) {
-        LOGGER.info("starting TestbedController");
-        TestbedController.getInstance();
-        LOGGER.info("starting NodeFlasherController");
-        new NodeFlasherController();
-        LOGGER.info("starting DataCollector");
-        final Thread dataCollector = new Thread(new DataCollector());
-        dataCollector.run();
+
+        if (PropertyReader.getInstance().getProperties().get("use.controller").equals("1")) {
+            LOGGER.info("starting TestbedController");
+            TestbedController.getInstance();
+        }
+        if (PropertyReader.getInstance().getProperties().get("use.nodeflasher").equals("1")) {
+            LOGGER.info("starting NodeFlasherController");
+            new NodeFlasherController();
+        }
+        if (PropertyReader.getInstance().getProperties().get("use.datacollector").equals("1")) {
+            LOGGER.info("starting DataCollector");
+            final Thread dataCollector = new Thread(new DataCollector());
+            dataCollector.run();
+        }
         LOGGER.info("up and running");
     }
 }
