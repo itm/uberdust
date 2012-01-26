8d55a38e6de7cf6d5436c9d188a2c1342e62359b
Dimitris Bousis <whantana@gmail.com>
2012-01-26 16:38:54 +0200

    introducing evaluation app

--- applications/EvaluationApp/src/main/java/eu/uberdust/communication/RestClient.java
+++ applications/EvaluationApp/src/main/java/eu/uberdust/communication/RestClient.java
@@ -1,4 +1,4 @@
-package eu.uberdust.communication.rest;
+package eu.uberdust.communication;
 
 import org.apache.log4j.Logger;
 
@@ -7,20 +7,18 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-/**
- * Implementation of a InsertReading client using Uberdust's REST interface.
- */
-public final class InsertReadingRestClient {
+public final class RestClient {
+
+    /**
+     * Static Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(RestClient.class);
+
 
     /**
      * static instance(ourInstance) initialized as null.
      */
-    private static InsertReadingRestClient ourInstance = null;
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = Logger.getLogger(InsertReadingRestClient.class);
+    private static RestClient ourInstance = null;
 
     /**
      * RestClient is loaded on the first execution of RestClient.getInstance()
@@ -28,10 +26,10 @@ public final class InsertReadingRestClient {
      *
      * @return ourInstance
      */
-    public static InsertReadingRestClient getInstance() {
-        synchronized (InsertReadingRestClient.class) {
+    public static RestClient getInstance() {
+        synchronized (RestClient.class) {
             if (ourInstance == null) {
-                ourInstance = new InsertReadingRestClient();
+                ourInstance = new RestClient();
             }
         }
         return ourInstance;
@@ -40,15 +38,14 @@ public final class InsertReadingRestClient {
     /**
      * Private constructor suppresses generation of a (public) default constructor.
      */
-    private InsertReadingRestClient() {
-        // empty constructor
+    private RestClient() {
     }
 
     /**
-     * Call RESTful Web Service.
+     * Call Remote  Rest Interface.
      *
-     * @param address , the address URL of service.
-     * @return this call result.
+     * @param address the address
+     * @return the return String
      */
     public String callRestfulWebService(final String address) {
         try {
@@ -66,18 +63,15 @@ public final class InsertReadingRestClient {
             }
             in.close();
 
+            if (address.contains("payload")) {
+                if (!inputLine.toString().contains("OK")) {
+                    throw new RuntimeException("Bad Response");
+                }
+            }
+            LOGGER.info(inputLine.toString());
             return inputLine.toString();
         } catch (final Exception e) {
             LOGGER.error(e);
-            if (e.getMessage().contains("406")) {
-                return "0\t0";
-            }
-            try {
-                LOGGER.info("Retrying...");
-                Thread.sleep(2000);
-            } catch (InterruptedException e1) {
-                LOGGER.error(e);
-            }
             callRestfulWebService(address);
         }
         return "0\t0";
