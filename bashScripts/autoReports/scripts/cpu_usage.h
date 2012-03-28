d86be8746eb8ee0adff245e5809f1ebee943e519
Dimitrios Amaxilatis <amaxilat@cti.gr>
2012-03-28 16:00:45 +0300

    close repo

--- bashScripts/autoReports/scripts/cpu_usage.h
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ../settings
-
-cpuusage=$(mpstat | grep "all" | awk '{print $4;}')
-
-echo "cpu:usage""="$cpuusage
-addReading "cpu:usage" $cpuusage
--- webapp/src/main/webapp/footer.jsp
+++
@@ -1,4 +0,0 @@
-<jsp:useBean id="time" scope="request" class="String"/>
-<c:if test="${time != ''}">
- page loaded in <c:out value="${time}"/> milliseconds
-</c:if>
